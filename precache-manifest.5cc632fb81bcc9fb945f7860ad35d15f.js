c94fed9f9895a30d843cb87302bc42887f685194
AlexunderJ <pozdrower@wp.pl>
2020-07-07 18:33:55 +0200

    Updates

---
+++ precache-manifest.5cc632fb81bcc9fb945f7860ad35d15f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14c7cc467c5496c8c12361c45f13255",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "84caca7f3a45fa379980",
+    "url": "/porfolio/static/css/main.169d8ec1.chunk.css"
+  },
+  {
+    "revision": "ef64957fd84fa66943c0",
+    "url": "/porfolio/static/js/2.0d96ae84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/porfolio/static/js/2.0d96ae84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84caca7f3a45fa379980",
+    "url": "/porfolio/static/js/main.bc908e91.chunk.js"
+  },
+  {
+    "revision": "0a5a44b9d39ddbc4a4db",
+    "url": "/porfolio/static/js/runtime-main.e6904c30.js"
+  },
+  {
+    "revision": "23cf9a8ec1b6221a906df76d9159d82c",
+    "url": "/porfolio/static/media/1234.23cf9a8e.jpg"
+  },
+  {
+    "revision": "ff08ac10f79322badb3cfb3e0c66dc98",
+    "url": "/porfolio/static/media/grafiti.ff08ac10.jpg"
+  },
+  {
+    "revision": "1e340f37b9acfdc1d078008f4a3e4099",
+    "url": "/porfolio/static/media/góry1.1e340f37.png"
+  }
+]);
