145ebcceaed69a07882522b80afdb32999ac1966
AlexunderJ <pozdrower@wp.pl>
2020-07-24 17:05:32 +0200

    router env URL

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -8,7 +8,7 @@ import Navigation from "./Navigation";
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <header>{<Header />}</header>
 
