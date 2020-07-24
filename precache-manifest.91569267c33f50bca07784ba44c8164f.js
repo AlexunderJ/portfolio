e44f0562040df10babcffd6715bd90bfa03484ae
AlexunderJ <pozdrower@wp.pl>
2020-07-24 16:59:25 +0200

    Updates

---
+++ precache-manifest.91569267c33f50bca07784ba44c8164f.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1ab07a2cea38412203bebb99e37d5f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "230df92702a441ba7d95",
+    "url": "/portfolio/static/css/main.68171821.chunk.css"
+  },
+  {
+    "revision": "868641bb7cd78f7a110d",
+    "url": "/portfolio/static/js/2.0bc181f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0bc181f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "230df92702a441ba7d95",
+    "url": "/portfolio/static/js/main.eb2a13a8.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "23cf9a8ec1b6221a906df76d9159d82c",
+    "url": "/portfolio/static/media/1234.23cf9a8e.jpg"
+  },
+  {
+    "revision": "fb347bd112d18aa729d293edbba3dcab",
+    "url": "/portfolio/static/media/7-Fashion Brand Wordmark OAKAO-01.fb347bd1.png"
+  },
+  {
+    "revision": "c63adfd0903529495084503d6258c602",
+    "url": "/portfolio/static/media/7-Fashion Brand Wordmark OAKAO-02.c63adfd0.png"
+  },
+  {
+    "revision": "f3d4035d1062a9d2224f3c4a833dc7d9",
+    "url": "/portfolio/static/media/7-Fashion Brand Wordmark OAKAO-05.f3d4035d.png"
+  },
+  {
+    "revision": "6ab783d5fed990b750a4c56b6bd58d02",
+    "url": "/portfolio/static/media/BRANDING.6ab783d5.svg"
+  },
+  {
+    "revision": "9ffb65e374e858a0c9ff78563160fdec",
+    "url": "/portfolio/static/media/Bunny-65.9ffb65e3.jpg"
+  },
+  {
+    "revision": "4d71baae9136a066b6d73412fa9ccd84",
+    "url": "/portfolio/static/media/Bunny-652.4d71baae.jpg"
+  },
+  {
+    "revision": "c57287b717ed3e4452dc019bb2f07e38",
+    "url": "/portfolio/static/media/Bunny-66.c57287b7.jpg"
+  },
+  {
+    "revision": "bcf1039bef87545dceab3fc20ce362a4",
+    "url": "/portfolio/static/media/IMG_0173.bcf1039b.jpg"
+  },
+  {
+    "revision": "b190e564e9c68204d9956295ffe12c5e",
+    "url": "/portfolio/static/media/LOGO.b190e564.svg"
+  },
+  {
+    "revision": "12c38df55571f6d8aae263ba7ca41594",
+    "url": "/portfolio/static/media/Magazine-Mockup-Showcase-3.12c38df5.jpg"
+  },
+  {
+    "revision": "9e2d7a0aed95a910b49c6ed73483ce0e",
+    "url": "/portfolio/static/media/Magazine-Mockup-Showcase-4.9e2d7a0a.jpg"
+  },
+  {
+    "revision": "d991a6119b500525859a2439e75b8aee",
+    "url": "/portfolio/static/media/Magazine-Mockup-Showcase-5.d991a611.jpg"
+  },
+  {
+    "revision": "45eb7b6d18fbd0a457c1fdd022472ce9",
+    "url": "/portfolio/static/media/ORH82L1.45eb7b6d.jpg"
+  },
+  {
+    "revision": "c602e96da132011656ccabb703f90dab",
+    "url": "/portfolio/static/media/POS.c602e96d.svg"
+  },
+  {
+    "revision": "fe55f57c5e44c3c606e1bde31e3ce560",
+    "url": "/portfolio/static/media/Tshirt-Mockup-PSD.fe55f57c.png"
+  },
+  {
+    "revision": "11dc577fd5885e99278ce4bea17dfa2d",
+    "url": "/portfolio/static/media/Untitled design.11dc577f.png"
+  },
+  {
+    "revision": "c70e41f81b5beddfdfe614facdfd930e",
+    "url": "/portfolio/static/media/Web 1920 – 5.c70e41f8.png"
+  },
+  {
+    "revision": "0bb5a9152e44135e51cfe3dce45683eb",
+    "url": "/portfolio/static/media/Web 1920 – 6.0bb5a915.png"
+  },
+  {
+    "revision": "90ce9a10dcd16650197120ec52e8b3a3",
+    "url": "/portfolio/static/media/Web 1920 – 7.90ce9a10.png"
+  },
+  {
+    "revision": "8a0bec962d830e066eba0ca971f91c2a",
+    "url": "/portfolio/static/media/Zasób 1.8a0bec96.png"
+  },
+  {
+    "revision": "1ad075f91e59ce58945a10bf5da97675",
+    "url": "/portfolio/static/media/abstrakt1.1ad075f9.png"
+  },
+  {
+    "revision": "839656c7bd0ed6bd94f622954073383a",
+    "url": "/portfolio/static/media/arrow.839656c7.svg"
+  },
+  {
+    "revision": "8221c6ea1510ec8324d1555a8d4b8980",
+    "url": "/portfolio/static/media/export logo-03.8221c6ea.png"
+  },
+  {
+    "revision": "2d59678560a4ffbe9e5429840eb04bb3",
+    "url": "/portfolio/static/media/export logo-09.2d596785.png"
+  },
+  {
+    "revision": "b4750f3a06ff196ea720e26eee98be1c",
+    "url": "/portfolio/static/media/export logo-15.b4750f3a.png"
+  },
+  {
+    "revision": "ff08ac10f79322badb3cfb3e0c66dc98",
+    "url": "/portfolio/static/media/grafiti.ff08ac10.jpg"
+  },
+  {
+    "revision": "1e340f37b9acfdc1d078008f4a3e4099",
+    "url": "/portfolio/static/media/góry1.1e340f37.png"
+  },
+  {
+    "revision": "3456fad9c0d3d83007feb4108829fc67",
+    "url": "/portfolio/static/media/hoodie1.3456fad9.jpg"
+  },
+  {
+    "revision": "4dce19e013b7413dcbf2b2e67feac172",
+    "url": "/portfolio/static/media/hoodie5.4dce19e0.jpg"
+  },
+  {
+    "revision": "abf3f9d134426130c9f1afe2df69c92f",
+    "url": "/portfolio/static/media/hoodie6.abf3f9d1.jpg"
+  },
+  {
+    "revision": "b815eaff8fb3e3bdbaab760c37698d53",
+    "url": "/portfolio/static/media/ilustracja1.b815eaff.png"
+  },
+  {
+    "revision": "26fbb99af349d2339373687918416570",
+    "url": "/portfolio/static/media/logo_full.26fbb99a.svg"
+  },
+  {
+    "revision": "42d465f9e3bf7894eeee583c8e877f69",
+    "url": "/portfolio/static/media/logo_name.42d465f9.svg"
+  },
+  {
+    "revision": "9b39142816f372e9156bb0413463c3ab",
+    "url": "/portfolio/static/media/logo_sygnet.9b391428.svg"
+  },
+  {
+    "revision": "a14faae4fa2c84e36c62054cb4649eaf",
+    "url": "/portfolio/static/media/napis.a14faae4.svg"
+  },
+  {
+    "revision": "8f9501e0882e7495d99abc2f42ec9fb8",
+    "url": "/portfolio/static/media/nowelogo obok copy1-10.8f9501e0.jpg"
+  },
+  {
+    "revision": "933ddd2688f7a02635c8088792becea5",
+    "url": "/portfolio/static/media/nowelogo obok copy1-11.933ddd26.jpg"
+  },
+  {
+    "revision": "37eb2254ce9a91a88b9eebd88d03ad30",
+    "url": "/portfolio/static/media/nowelogo obok copy1-13.37eb2254.jpg"
+  },
+  {
+    "revision": "94e804cb6ccc644ca56eea141c0b2538",
+    "url": "/portfolio/static/media/preview.94e804cb.jpg"
+  },
+  {
+    "revision": "f549c2566d69a1c9c8ee91bd1d34297a",
+    "url": "/portfolio/static/media/pulpit1.f549c256.png"
+  },
+  {
+    "revision": "255794554a8dda2bdd3b38ca689c5d83",
+    "url": "/portfolio/static/media/pulpit2 – 1.25579455.png"
+  },
+  {
+    "revision": "69eb861ef39b92b21d620663b7c5f2b1",
+    "url": "/portfolio/static/media/salad.69eb861e.jpg"
+  },
+  {
+    "revision": "14d8c899e5d97ec4a2cc95d30017ca97",
+    "url": "/portfolio/static/media/salad2.14d8c899.jpg"
+  },
+  {
+    "revision": "0785ebe84045c2f486a49d38535b0f03",
+    "url": "/portfolio/static/media/webdesign.0785ebe8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/porfolio/precache-manifest.69fc025d2421212ce0fcd1b9a4546dde.js"
+  "/portfolio/precache-manifest.91569267c33f50bca07784ba44c8164f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/porfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
